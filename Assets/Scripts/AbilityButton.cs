5c6dc2b328cfb34428281dc15e5bd9d34d805acd
alexsapo17 <alexsaporito17@gmail.com>
2024-02-29 21:32:42 +0100

    Ripristinato lo stato dei file specifici al commit precedente3

--- Assets/Scripts/AbilityButton.cs
+++ Assets/Scripts/AbilityButton.cs
@@ -44,34 +44,19 @@ public class AbilityButton : MonoBehaviour
             Debug.Log("Pulsante abilità acquistato con successo!");
             isUnlocked = true;
 
-            GameObject animatedImagePrefab = Resources.Load<GameObject>("HitMiscATest");
-if (animatedImagePrefab != null)
-{
-    Canvas canvas = FindObjectOfType<Canvas>();
-    Camera camera = canvas.worldCamera; // Ottieni la camera usata dal Canvas
+            GameObject animatedImagePrefab = Resources.Load<GameObject>("GoldExplosionAnim");
+            if (animatedImagePrefab != null)
+            {
+                Canvas canvas = FindObjectOfType<Canvas>();
+                Camera camera = canvas.worldCamera; // Ottieni la camera usata dal Canvas
+                
+                // Calcola la rotazione per far sì che il prefab guardi verso la camera.
+                Quaternion rotationTowardsCamera = Quaternion.LookRotation(camera.transform.forward);
 
-    // Ottieni la posizione del bottone rispetto alla camera
-    Vector3 buttonPositionOnScreen = camera.WorldToScreenPoint(buyButton.transform.position);
-
-    // Calcola una posizione lungo la direzione della camera, a una certa distanza davanti ad essa
-    Vector3 positionInFrontOfCamera = camera.ScreenToWorldPoint(new Vector3(buttonPositionOnScreen.x, buttonPositionOnScreen.y, camera.nearClipPlane + 0.5f)); // Sostituisci '1' con la distanza che preferisci
-
-    // La rotazione che punta verso la camera
-    Quaternion rotationTowardsCamera = Quaternion.LookRotation(camera.transform.position - positionInFrontOfCamera);
-
-    // Istanzia il prefab con la posizione e la rotazione calcolate
-    GameObject animatedImageInstance = Instantiate(animatedImagePrefab, positionInFrontOfCamera, rotationTowardsCamera);
-
-    // Assicurati che l'oggetto sia parented correttamente se necessario
-    // Potresti dover parentarlo al canvas o ad un altro oggetto, a seconda delle tue esigenze
-animatedImageInstance.transform.SetParent(this.transform, true);
-animatedImageInstance.transform.localScale = new Vector3(1, 1, 1); // Imposta la scala desiderata
-
-    // Distrugge l'oggetto dopo 1 secondo
-    Destroy(animatedImageInstance, 1f);
-
-
-}
+                // Istanzia il prefab con la rotazione calcolata
+                GameObject animatedImageInstance = Instantiate(animatedImagePrefab, buyButton.transform.position, rotationTowardsCamera, this.transform);
+                Destroy(animatedImageInstance, 1f);
+            }
             else
             {
                 Debug.LogError("Il prefab AnimatedImagePrefab non è stato trovato nella cartella Resources.");
