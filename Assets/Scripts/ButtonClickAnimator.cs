00c4ba98aa89eacc4770a4a5c80a79006e706988
alexsapo17 <alexsaporito17@gmail.com>
2024-02-29 22:54:05 +0100

    ripristino tutti script tranne lootboxmanager

--- Assets/Scripts/ButtonClickAnimator.cs
+++ Assets/Scripts/ButtonClickAnimator.cs
@@ -11,13 +11,15 @@ public class ButtonClickAnimator : MonoBehaviour
 
     private Vector3 originalScale;
     private CanvasGroup canvasGroup;
+    private Animator animator; // Variabile per memorizzare il riferimento all'Animator
 
     void Start()
     {
         originalScale = transform.localScale;
         canvasGroup = GetComponent<CanvasGroup>();
-        if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        animator = GetComponent<Animator>(); // Ottieni il riferimento all'Animator
 
+        if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
     }
 
     public void OnButtonClick()
@@ -27,7 +29,11 @@ public class ButtonClickAnimator : MonoBehaviour
 
     IEnumerator AnimateButton()
     {
-
+        // Disattiva l'Animator se è presente
+        if (animator != null)
+        {
+            animator.enabled = false;
+        }
 
         float time = 0;
         Time.timeScale = 1;
@@ -36,15 +42,13 @@ public class ButtonClickAnimator : MonoBehaviour
             transform.localScale = Vector3.Lerp(originalScale, enlargedScale, time / animationDuration);
             time += Time.deltaTime;
             yield return null;
-
         }
 
-
-        // Ripristina lo stato originale e attiva l'evento onclick
+        // Ripristina lo stato originale e attiva l'evento onClick
         transform.localScale = originalScale;
         canvasGroup.alpha = 1;
         
-        if (onClick != null)
+        if (onClick != null) 
         {
             onClick.Invoke();
         }
@@ -53,5 +57,10 @@ public class ButtonClickAnimator : MonoBehaviour
             Debug.Log("Attenzione: onClick è null.");
         }
 
+        // Riattiva l'Animator dopo l'animazione, se necessario
+        if (animator != null)
+        {
+            animator.enabled = true;
+        }
     }
 }
--- Assets/Scripts/CameraFollow1.cs
+++ Assets/Scripts/CameraFollow1.cs
@@ -1,14 +1,47 @@
 ﻿using UnityEngine;
-
+using UnityEngine.EventSystems; // Necessario per interagire con EventTrigger
+using System.Collections.Generic;
+using System.Linq;
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // Il target che la camera deve seguire
-    public Vector3 offset = new Vector3(0, 5, -10); // Offset della camera rispetto al target
-    public float fixedYPosition = 10.0f; // Imposta questo valore all'altezza Y desiderata
-    public float smoothSpeed = 5.0f; // Regola la velocità di smorzamento
-    public float tiltAngleX = 20.0f; // Angolo di inclinazione lungo l'asse X
+    public Vector3 offset = new Vector3(0, 5, -10);
+    public float fixedYPosition = 10.0f;
+    public float smoothSpeed = 5.0f;
+    public float tiltAngleX = 20.0f;
+public float touchSensitivity = 0.5f; // Regola questa variabile per controllare la sensibilità del trascinamento
 
-    // Metodo per impostare il target della camera.
+    public float yTouchSensitivity = 0.1f; // Regola la sensibilità del movimento sull'asse Y
+public float zTouchSensitivity = 0.1f; // Regola la sensibilità del movimento sull'asse Z
+public float maxXOffset = 5f; // Limite massimo di spostamento sull'asse X
+public float maxYOffset = 5f; // Limite massimo di spostamento sull'asse Y
+public float maxZOffset = 5f; // Limite massimo di spostamento sull'asse Z
+
+    private Vector3 initialOffset;
+    private Transform target;
+    private bool isDragging = false;
+    private Vector2 lastTouchPosition; // Aggiunto per tenere traccia dell'ultima posizione del touch
+    private int dragFingerId = -1; // Aggiungi questa variabile per tenere traccia dell'ID del tocco
+
+    void Start()
+    {
+        initialOffset = offset;
+        GameObject inputPanel = GameObject.Find("InputPanel");
+        if (inputPanel != null)
+        {
+            EventTrigger trigger = inputPanel.GetComponent<EventTrigger>();
+            if (trigger != null)
+            {
+                EventTrigger.Entry entryDown = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
+                entryDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+                trigger.triggers.Add(entryDown);
+
+                EventTrigger.Entry entryUp = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
+                entryUp.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
+                trigger.triggers.Add(entryUp);
+            }
+        }
+    }
+
     public void SetTarget(GameObject target)
     {
         if (target != null)
@@ -17,24 +50,108 @@ public class CameraFollow : MonoBehaviour
         }
     }
 
+    private void OnPointerDown(PointerEventData data)
+{
+    // Verifica se il tocco non è sopra un elemento UI utilizzando la posizione dell'evento
+    if (!IsPointerOverUIObject(data.position))
+    {
+        isDragging = true;
+        // Imposta l'ultima posizione del touch basandosi sulla posizione dell'evento
+        lastTouchPosition = data.position;
+    }
+}
+
+
+    private void OnPointerUp(PointerEventData data)
+    {
+        isDragging = false;
+    }
+
+void Update()
+{
+    if (Input.touchCount > 0)
+    {
+        if (!isDragging)
+        {
+            // Cerca un tocco che inizi al di fuori degli elementi UI per iniziare il trascinamento
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began && !IsPointerOverUIObject(touch.position))
+                {
+                    dragFingerId = touch.fingerId;
+                    lastTouchPosition = touch.position;
+                    isDragging = true;
+                    break; // Interrompi il ciclo una volta trovato un tocco valido
+                }
+            }
+        }
+        else
+        {
+            // Gestisci il tocco che sta trascinando la camera
+            Touch touch = Input.touches.FirstOrDefault(t => t.fingerId == dragFingerId);
+if (touch.phase == TouchPhase.Moved)
+{
+    Vector2 deltaTouch = touch.position - lastTouchPosition;
+    lastTouchPosition = touch.position;
+
+    // Assicurati che il movimento sia proporzionale e coerente in entrambe le direzioni
+    Vector3 touchMovement = new Vector3(
+        deltaTouch.x * touchSensitivity,
+        -Mathf.Abs(deltaTouch.x) * yTouchSensitivity, // Usa Mathf.Abs per mantenere la direzione
+        -Mathf.Abs(deltaTouch.x) * zTouchSensitivity
+    );
+
+    // Calcola il nuovo offset considerando i limiti
+    Vector3 potentialOffset = offset + touchMovement * Time.deltaTime;
+    offset = new Vector3(
+        Mathf.Clamp(potentialOffset.x, initialOffset.x - maxXOffset, initialOffset.x + maxXOffset),
+        Mathf.Clamp(potentialOffset.y, initialOffset.y - maxYOffset, initialOffset.y + maxYOffset),
+        Mathf.Clamp(potentialOffset.z, initialOffset.z - maxZOffset, initialOffset.z + maxZOffset)
+    );
+}
+
+
+
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                dragFingerId = -1;
+            }
+        }
+    }
+    else
+    {
+        isDragging = false;
+        dragFingerId = -1;
+    }
+
+    if (!isDragging)
+    {
+        offset = Vector3.Lerp(offset, initialOffset, smoothSpeed * Time.deltaTime);
+    }
+}
+
+ 
     void LateUpdate()
     {
         if (target != null)
         {
             Vector3 offsetRotated = target.TransformDirection(offset);
             Vector3 desiredPosition = target.position + offsetRotated;
-
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, new Vector3(desiredPosition.x, fixedYPosition, desiredPosition.z), smoothSpeed * Time.deltaTime);
-
-            // Imposta la posizione della camera
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, new Vector3(desiredPosition.x, desiredPosition.y, desiredPosition.z), smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
-
-            // Imposta la rotazione della camera per guardare il target con l'inclinazione specificata
             transform.LookAt(target);
-            
-            // Aggiunge un'inclinazione lungo l'asse X alla camera
             Vector3 currentRotation = transform.eulerAngles;
             transform.rotation = Quaternion.Euler(currentRotation.x + tiltAngleX, currentRotation.y, currentRotation.z);
         }
     }
+   private bool IsPointerOverUIObject(Vector2 touchPos)
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(touchPos.x, touchPos.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
 }
